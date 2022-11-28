b0aa69dcd195bdbb37d78fe13ce84f5be57630a1
Tian Wong <tianwong02@gmail.com>
2022-11-27 20:41:35 -0800

    rollback match for <3.10, init demo routine

--- web_app/README.md
+++ web_app/README.md
@@ -1,7 +1,11 @@
-# Setup
+# Spotify Analysis Flask Application
 
-Relying on the [Spotipy Python library](https://spotipy.readthedocs.io/en/2.21.0/), our project requires some minimal setup to get up and running. While not integral for the ML capabilities of the project, the integration does allow for live querying of Spotify data and make our application much more applicable to the needs outlined in our proposal.
+## Background
 
-Looking at [app.py](./app.py), we can see that a SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET are missing. These can be obtained by going to https://developer.spotify.com/dashboard/ and creating a new app.
+Relying on the [Spotipy Python library](https://spotipy.readthedocs.io/en/2.21.0/), our project requires some minimal setup to get live Spotify querying up and running. While not integral for the ML capabilities of the project, the integration does allow for testing on arbitrary data and enables our application to be much more applicable to the needs outlined in our proposal. For demonstration purposes, a `Demo` option has also been made available to run the model without the following setup instructions.
 
-Once these two are configured, the last step is to set a redirect URI. A redirect has already been set in [app.py](./app.py), so all one needs to do is to copy and paste it into the `Redirect URI` field under the `Edit Settings` tab.
+## Setup
+
+Looking at [app.py](./app.py), we can see that a SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET are missing. These can be obtained by going to https://developer.spotify.com/dashboard/ and creating a new app. Also be sure to set LIVE_QUERY=True once done.
+
+Once these are configured, the last step is to set a redirect URI on your Spotify app dashboard. A redirect has already been set in [app.py](./app.py), so all one needs to do is to copy and paste it into the `Redirect URI` field under the `Edit Settings` tab.
--- web_app/app.py
+++ web_app/app.py
@@ -1,17 +1,22 @@
+import json
 from flask import Flask, render_template, request
 import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
+LIVE_QUERY = False
 SPOTIPY_CLIENT_ID = ''
 SPOTIPY_CLIENT_SECRET = ''
 SPOTIPY_REDIRECT = 'http://127.0.0.1:9090/callback'
 
 app = Flask(__name__)
 scope = 'user-top-read,user-library-read,user-read-recently-played'
-oauth = SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT)
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET), auth_manager=oauth)
+oauth = None
+sp = None
+if LIVE_QUERY:
+    oauth = SpotifyOAuth(scope=scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT)
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET), auth_manager=oauth)
 
 link_pattern = r"https:\/\/open.spotify.com\/([^\/]*)\/([^\?]*)"
 
@@ -21,40 +26,44 @@ def index():
 
 @app.route('/submission', methods=['POST'])
 def index_post():
-    url = request.form['text']
-    m = re.match(link_pattern, url)
-    if not m:
-        return "Expected track, album, playlist, or artist link"
-    attributes = collect_attributes(m.group(1), m.group(2))
+    attributes = None
+    if request.form['submit'] == "live":
+        url = request.form['text']
+        m = re.match(link_pattern, url)
+        if not m:
+            return "Expected track, album, playlist, or artist link"
+        attributes = collect_attributes(m.group(1), m.group(2))
+    elif request.form['submit'] == "demo":
+        with open("./submission.json") as f:
+            attributes = json.load(f)
     return attributes
 
 def collect_attributes(query_type, input_id):
     attributes = []
-    match query_type: # python 3.10 feature
-        case "track":
-            track_att = track_attributes(input_id)
-            track_att["name"] = sp.track(input_id)["name"]
+    if query_type == "track":
+        track_att = track_attributes(input_id)
+        track_att["name"] = sp.track(input_id)["name"]
+        attributes.append(track_att)
+    elif query_type == "album": # annoying that albums, playlists, and artists arrange track info differently
+        track_dict = sp.album_tracks(input_id)
+        for track in track_dict["items"]:
+            track_att = track_attributes(track["id"])
+            track_att["name"] = track["name"]
             attributes.append(track_att)
-        case "album": # annoying that albums, playlists, and artists arrange track info differently
-            track_dict = sp.album_tracks(input_id)
-            for track in track_dict["items"]:
-                track_att = track_attributes(track["id"])
-                track_att["name"] = track["name"]
-                attributes.append(track_att)
-        case "playlist":
-            track_dict = sp.playlist_tracks(input_id)
-            for track in track_dict["items"]:
-                track_att = track_attributes(track["track"]["id"])
-                track_att["name"] = track["track"]["name"]
-                attributes.append(track_att)
-        case "artist":
-            track_dict = sp.artist_top_tracks(input_id)
-            for track in track_dict["tracks"]:
-                track_att = track_attributes(track["id"])
-                track_att["name"] = track["name"]
-                attributes.append(track_att)
-        case _:
-            return f"Expected track, album, playlist, or artist; got {query_type}"
+    elif query_type == "playlist":
+        track_dict = sp.playlist_tracks(input_id)
+        for track in track_dict["items"]:
+            track_att = track_attributes(track["track"]["id"])
+            track_att["name"] = track["track"]["name"]
+            attributes.append(track_att)
+    elif query_type == "artist":
+        track_dict = sp.artist_top_tracks(input_id)
+        for track in track_dict["tracks"]:
+            track_att = track_attributes(track["id"])
+            track_att["name"] = track["name"]
+            attributes.append(track_att)
+    else:
+        return f"Expected track, album, playlist, or artist; got {query_type}"
     return attributes
 
 def track_attributes(trid):
